d1a2cc78a00ff62d737b4b28b53af3239621757f
Elvis <s49188705@gmail.com>
2022-09-27 19:42:05 +0300

    Completed Assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQuestion, updatedQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDelete(id) {
+    deleteQuestion(id);
+  }
+  function handleChange(newCorrectIndex) {
+    updatedQuestion(question, newCorrectIndex);
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -11,13 +18,15 @@ function QuestionItem({ question }) {
 
   return (
     <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
+      <h4>Question:{id}</h4>
+      <h5>Prompt:{prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={(event) => handleChange(event.target.value)}defaultValue={correctIndex}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => handleDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((resp) => resp.json())
+      .then((questions) => {
+        setQuestions(questions);
+      });
+  });
+
+  function deleteQuestion(questionId) {
+  const serverOption = {
+      method: "DELETE",
+    }
+
+      fetch(`http://localhost:4000/questions/${questionId}`, serverOption)
+      .then((resp) => resp.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((question) => {
+          return question.id !== questionId;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  function updatedQuestion(question, newCorrectIndex) {
+    const serverOption = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({correctIndex: newCorrectIndex}),
+    };
+
+    fetch(`http://localhost:4000/questions/${question.id}`, serverOption)
+      .then((resp) => resp.json())
+      .then((newQuestion) => {
+        const updatedQuestions = questions.map((ques) => {
+          if (ques.id === question.id) {
+            return newQuestion;
+          }
+          return question;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  //Rendering the questions
+  const QuestionList = questions.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        question={question}
+        updatedQuestion={updatedQuestion}
+        deleteQuestion={deleteQuestion}
+      />
+    );
+  });
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{QuestionList}</ul>
     </section>
   );
 }
